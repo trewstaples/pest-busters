699e8e1b136aa4ac63687d584167bb0a16460ece
LAlex <trewstaples@gmail.com>
2022-10-21 18:07:30 +0300

    Add rules

--- public/js/header.js
+++ public/js/header.js
@@ -1,25 +1,3 @@
-// const aboutAnchor = document.querySelector('a[href*="#about"]');
-
-// aboutAnchor.addEventListener('click', (evt) => {
-//   evt.preventDefault();
-//   const blockID = aboutAnchor.getAttribute('href');
-//   document.querySelector('' + blockID).scrollIntoView({
-//     behavior: 'smooth',
-//     block: 'start',
-//   });
-// });
-
-// const servicesAnchor = document.querySelector('a[href*="#services"]');
-
-// servicesAnchor.addEventListener('click', (evt) => {
-//   evt.preventDefault();
-//   const blockID = servicesAnchor.getAttribute('href');
-//   document.querySelector('' + blockID).scrollIntoView({
-//     behavior: 'smooth',
-//     block: 'start',
-//   });
-// });
-
 const anchors = ['about', 'services', 'entities', 'individuals', 'portfolio', 'footer'];
 
 const createLinkScroll = (elementHref) => {
@@ -31,6 +9,7 @@ const createLinkScroll = (elementHref) => {
     document.querySelector('' + blockID).scrollIntoView({
       behavior: 'smooth',
       block: 'start',
+      inline: 'nearest',
     });
   });
 };
