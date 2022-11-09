8682ed51dd7f364a5f98d78c75b558c74e96cabe
LAlex <trewstaples@gmail.com>
2022-11-09 22:43:00 +0300

    Add server realization

--- public/js/main.js
+++ public/js/main.js
@@ -26,18 +26,94 @@ ANCHORS.forEach((anchor) => scrollToChapter(anchor));
 
 const contactFormBtn = document.querySelector('.contact-form-btn');
 const formContainer = document.querySelector('.form-container');
-const contactForm = document.querySelector('.contact-form');
 const closeBtn = document.querySelector('.close-btn');
 
 contactFormBtn.addEventListener('click', () => {
-  formContainer.style.display = 'block';
+  formContainer.style.display === 'none' ? (formContainer.style.display = 'block') : (formContainer.style.display = 'none');
 });
 
 closeBtn.addEventListener('click', () => {
   formContainer.style.display = 'none';
 });
 
-contactForm.addEventListener('submit', (evt) => {
-  evt.preventDefault();
-  console.log('submit clicked');
+const contactForm = document.querySelector('.contact-form');
+
+let firstName = document.getElementById('name');
+let number = document.getElementById('number');
+let email = document.getElementById('email');
+let message = document.getElementById('message');
+
+firstName.addEventListener('input', (evt) => {
+  const isFirstNameValid = /^[А-ЯЁ ,.'-][а-яё ,.'-]+$/i.test(evt.target.value);
+  if (!isFirstNameValid) {
+    firstName.reportValidity();
+    firstName.style.border = '3px solid red';
+    return;
+  }
+  firstName.setCustomValidity('');
+  firstName.style.border = 'none';
+});
+
+number.addEventListener('input', (evt) => {
+  const isNumberValid = /^((\+7|7|8)+([0-9]){10})$/.test(evt.target.value);
+  if (!isNumberValid) {
+    number.reportValidity();
+    number.style.border = '3px solid red';
+    return;
+  }
+  number.style.border = 'none';
+  number.setCustomValidity('');
+});
+
+email.addEventListener('input', (evt) => {
+  const isEmailValid = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(evt.target.value);
+  if (!isEmailValid) {
+    email.reportValidity();
+    email.style.border = '3px solid red';
+    return;
+  }
+  email.setCustomValidity('');
+  email.style.border = 'none';
+});
+
+message.addEventListener('change', (evt) => {
+  const isMessageValid = /^[а-яА-ЯёЁa-zA-Z0-9]+$/.test(evt.target.value);
+  if (!isMessageValid) {
+    message.setCustomValidity('Разрешены только буквы и цифры');
+    message.reportValidity();
+    message.style.border = '3px solid red';
+    return;
+  }
+  message.setCustomValidity('');
+  message.style.border = 'none';
+});
+
+contactForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  let formData = {
+    name: firstName.value.toString(),
+    email: email.value.toString(),
+    number: number.value.toString(),
+    message: message.value.toString(),
+  };
+
+  let xhr = new XMLHttpRequest();
+  xhr.open('POST', '/');
+  xhr.setRequestHeader('content-type', 'application/json');
+
+  xhr.onload = function () {
+    if (xhr.responseText == 'success') {
+      alert('Email sent');
+      firstName.value = '';
+      email.value = '';
+      number.value = '';
+      message.value = '';
+      formContainer.style.display = 'none';
+    } else {
+      alert('Произошла ошибка. Попробуйте снова');
+    }
+  };
+
+  xhr.send(JSON.stringify(formData));
 });
--- server.js
+++ server.js
@@ -1,15 +1,43 @@
 const express = require('express');
 const app = express();
 
+const nodemailer = require('nodemailer');
+
 const PORT = process.env.PORT || 3000;
 
 //Middleware
 app.use(express.static('public'));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.post('/', (req, res) => {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: req.body.email,
+    to: process.env.EMAIL,
+    subject: `Сообщение от ${req.body.email}`,
+    text: `
+    От: ${req.body.email}
+    Номер телефона: ${req.body.number}
+    Текст обращения: ${req.body.message}
+    `,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      res.send('error');
+    } else {
+      res.send('success');
+    }
+  });
 });
